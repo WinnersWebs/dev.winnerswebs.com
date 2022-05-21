ad87298110037607cc68445f0e86b7d4aba46d94
James Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2022-05-21 17:43:35 -0400

    Update Services.jsx
    remove comment

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -19,7 +19,7 @@ export const Services = () => {
         <div className='bg-semidark-green text-white p-3 row-start-4 col-start-2 rounded-2xl shadow-xl'>
           <h3 className='text-amber-400'>Conversational Content</h3>
           <p>A chance for you to share your story and offerings, and a chance for your employees to share their successes.</p>
-          {/* line 21 contains revised tagline, planning to move all taglines above the fold, unsure whether these HTML comments are valid due to syntax highlighting inconsistency */}
+          {/* line 21 contains revised tagline, planning to move all taglines above the fold */}
           <p>People do business with people they know, like and trust. You've probably heard that one before. There's no better way for your audience to know, like and trust you than to let them hear you and your employees talk. There's no better way for top potential employees to understand why they should work for you — and that's by hearing your employees speak about their successes. Winners Webs will help. First we'll get to know one another. Then we'll talk with you about your story and offerings and with your employees about their successes. Last we'll select the best of it and show people.</p>
         </div>
     </div>

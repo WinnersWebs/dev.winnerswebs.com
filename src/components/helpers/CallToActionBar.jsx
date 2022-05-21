d42d6056907cdab0b0d7a36058b9eb9f8175b0ea
James Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2022-05-21 17:47:19 -0400

    Update CallToActionBar.jsx
    modify elems and cta liner

--- src/components/helpers/CallToActionBar.jsx
+++ src/components/helpers/CallToActionBar.jsx
@@ -5,11 +5,11 @@ const CallToActionBar = () => {
     <div className='bg-amber-400'>
         <div className='container mx-auto py-14 grid grid-cols-3 '>
             <div className='text-gray'>
-                <h6 className='text-4xl'>Get started with your project!</h6>
-                <p className='text-2xl'>Let us know a few details about your business needs and we'll get started</p>
+                <span className='text-4xl'>Ready — Set — Win?</span>
+                <span className='text-2xl'>Let us know a few details about your business needs and we'll get started</span>
             </div>
             <div className='col-start-3 flex items-center justify-center'>
-                <button className='bg-transparent px-5 py-3 text-xl font-bold rounded-md border-2 text-gray border-gray-700'>CONTACT US TODAY</button>
+                <button className='bg-transparent px-5 py-3 text-xl font-bold rounded-md border-2 text-gray border-gray-700'>CONTACT US</button>
             </div>
         </div>
     </div>

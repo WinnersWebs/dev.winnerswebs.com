e605f615e2ee8699ceb4dd817f2a8cbc209a03a1
James Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2022-05-21 17:17:09 -0400

    Update Footer.jsx
    should contact <ol> links be wrapped in <nav>? i believe not

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,12 +17,12 @@ export const Footer = () => {
           </ol>
         </div>
         <div>
-          <h6 className='text-white text-3xl mb-4'>Contact</h6>
-          <ol className='text-gray-500 text-xl flex flex-col gap-1'>
+          <ol className='text-gray-500 text-xl flex flex-col gap-1' id="footerContactOl">
+            <label className='text-white text-3xl mb-4' for="footerContactOl">Contact</label>
             <li><a className="hover:text-gray-400" href="mailto:hey@winnerswebs.com">Email: chris@winnerswebs.com</a></li>
             <li><a className="hover:text-gray-400" href="tel:3077575617">Tel: 3077575617</a></li>
             <li className="flex gap-3 mt-2">
-                  <a className="hover:text-gray-400" href="https://www.facebook.com/winnerswebs/">{iconFacebook}</a>
+              <a className="hover:text-gray-400" href="https://www.facebook.com/winnerswebs/">{iconFacebook}</a>
               <a className="hover:text-gray-400" href="https://www.instagram.com/winnerswebs/">{iconInstagram}</a>
               <a className="hover:text-gray-400" href="https://www.twitter.com/winnerswebs/">{iconTwitter}</a>
               <a className="hover:text-gray-400" href="https://www.linkedin.com/company/winnerswebs/">{iconLinkedin}</a>

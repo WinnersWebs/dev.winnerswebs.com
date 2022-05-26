431637ddebfc7bdcf473d6a45fa5c577c91dddec
Brayan Leyva <brayanleyvag@gmail.com>
2022-05-26 18:12:24 -0500

    logos to services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import defaultImg from '../img/default.png'
-import ccLogo from '../img/CC-logo.png'
-import nfwLogo from '../img/nfw-logo.png'
+import ccLogo from '../img/Conversational_Content-logo.png'
+import nfwLogo from '../img/Nothing_Fancy_Websites-logo.png'
+import pcaLogo from '../img/Pretty_Cool_Ads-logo.png'
+import rcccLogo from '../img/Really_Cool_Coaching_and_Consulting-logo.png'
 import { iconOpenIn } from './helpers/icons'
 
 export const Services = () => {
@@ -21,10 +23,10 @@ export const Services = () => {
       </div>
       <div className='flex flex-col-reverse items-center gap-8 bg-semidark-green rounded-2xl py-10 mx-5 text-justify lg:flex-row-reverse lg:p-10 lg:text-center'>
         <div className='w-2/3 flex flex-col justify-center gap-2 items-center'>
-          <img src={defaultImg} alt='Conversational Content logo' className='h-24 w-24'></img>
+          <img src={pcaLogo} alt='Conversational Content logo' className='h-24 w-24'></img>
           <h3 className='text-amber-400 text-4xl text-center'>Pretty Cool Ads</h3>
           <span className='text-gray-400 text-2xl'>Ads aren't most people's favorite things, so we try to be cool <span className="whitespace-nowrap">about it.</span></span>
-          <p className='text-white text-xl'>>Ads benefit you because they increase sales. Ads benefit your customers because they're shown content tailored to them in a way not possible through organic social media content. If you're a little confused about this, let's talk. If you know exactly what we mean, let's talk. 😎</p>
+          <p className='text-white text-xl'>Ads benefit you because they increase sales. Ads benefit your customers because they're shown content tailored to them in a way not possible through organic social media content. If you're a little confused about this, let's talk. If you know exactly what we mean, let's talk. 😎</p>
         </div>
         <div className='px-5 lg:px-0 md:w-1/2 lg:w-1/3'>
           <img className='rounded-xl' src={defaultImg}></img>
@@ -32,7 +34,7 @@ export const Services = () => {
       </div>
       <div className='flex flex-col-reverse items-center gap-8 bg-semidark-green rounded-2xl py-10 mx-5 text-justify lg:flex-row lg:p-10 lg:text-center'>
         <div className='w-2/3 flex flex-col justify-center gap-2 items-center'>
-          <img src={defaultImg} alt='Conversational Content logo' className='h-24 w-24'></img>
+          <img src={rcccLogo} alt='Conversational Content logo' className='h-24 w-24'></img>
             <h3 className='text-amber-400 text-4xl'>Really Cool Coaching & Consulting</h3>
             <span className='text-gray-400 text-2xl'>Whether you need to plan or need a motivational boost, we'll help you through the important parts of <span className="whitespace-nowrap">social media.</span></span>
             <p className='text-white text-xl'>Ever feel unmotivated to make posts and reply to comments on social media? Or like you're not sure how to plan your approach on social? With Really Cool Coaching and Consulting by Winners Webs we'll work together to ensure you're feeling prepared and effective. Your social media audience will appreciate your efforts, and your sales will reflect it.</p>

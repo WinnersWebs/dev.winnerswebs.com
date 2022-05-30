02e656524bf89476cfb56370be098b98aec64e2c
James Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2022-05-30 18:12:58 -0400

    fix hover color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
                 <h2 className='overflow-y-hidden text-white text-4xl text-center md:text-5xl md:text-left lg:text-6xl lg:mb-8'>Helping you <span className='text-amber-400'>win</span> on the <span className='text-amber-400'>web</span>.</h2>
                 <p className='overflow-y-hidden text-2xl text-neutral-200 lg:text-3xl'>You want to profit and you know how important marketing online is. But you know that to profit as a small business you have to reach people in increasingly more personable ways on the Internet. We're Winners Webs and we help small local businesses win on <span className="whitespace-nowrap">the web.</span></p>
                 <a href='#contactForm' >
-                  <button className='hover:amber-300 bg-amber-400 px-8 py-4 text-xl font-bold rounded-md text-slate-700 mt-5'>Contact Us</button>
+                  <button className='hover:text-amber-300 bg-amber-400 px-8 py-4 text-xl font-bold rounded-md text-slate-700 mt-5'>Contact Us</button>
                 </a>
             </div>
         </div>

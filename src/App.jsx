ca01e5ea537370b6125adf26c71e0d9219280cdc
Brayan Leyva <brayanleyvag@gmail.com>
2022-06-01 15:09:24 -0500

    Format & doble quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
-import './App.css'
-import { Header } from './components/Header'
-import { Hero } from './components/Hero'
-import { Services } from './components/Services'
-import { Footer } from './components/Footer'
-import { Results } from './components/Results'
-import { Form } from './components/Form'
-import CallToActionBar from './components/helpers/CallToActionBar'
-import { ToTopBtn } from './components/helpers/ToTopBtn'
+import "./App.css";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { Services } from "./components/Services";
+import { Footer } from "./components/Footer";
+import { Results } from "./components/Results";
+import { Form } from "./components/Form";
+import CallToActionBar from "./components/helpers/CallToActionBar";
+import { ToTopBtn } from "./components/helpers/ToTopBtn";
 
 function App() {
-
   return (
-    <div className='bg-dark-green'>
+    <div className="bg-dark-green">
       <div className=" bg-[url('./img/svg-bg.svg')] bg-dark-green bg-cover">
-        <div className='container mx-auto flex flex-col items-center'>
-          <Header/>
-          <Hero/>
+        <div className="container mx-auto flex flex-col items-center">
+          <Header />
+          <Hero />
         </div>
-        <ToTopBtn/>
+        <ToTopBtn />
         <div>
-          <Results/>
+          <Results />
         </div>
       </div>
-      <div className='bg-dark-green'>
-        <div className='container mx-auto'>
-          <Services/>
+      <div className="bg-dark-green">
+        <div className="container mx-auto">
+          <Services />
         </div>
-          <CallToActionBar/>
-          <Form/>
-          <Footer/>
+        <CallToActionBar />
+        <Form />
+        <Footer />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
